e5c5500985b7794107822aaeab4564f0cdb58b62
Nediya-MD <Nediyaboy@gmail.com>
2025-11-11 19:20:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "YcBlCByC#hUNaVMVBqpAVvS6zgpyqQpIMh8fD-WjdauAKToF5IRM",
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94715865463",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94761207139",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
-ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY NEDIYA <NOW ALIVE> ",
 };
